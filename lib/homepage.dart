0e289462cd1a26b15a24d8c9f0d900f3913cce6d
Jang Hanna <wkdgkssk204@naver.com>
2023-07-21 11:46:48 +0900

    update file

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -7,63 +7,62 @@ class HomePage extends StatelessWidget {
     return DefaultTabController(
       length: 4,
       child: Scaffold(
-        appBar: AppBar(
-          title: const Text('T A B  B A R'),
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.search),
-              onPressed: () {
-                print('search');
-              },
-            ),
-          ],
-        ),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: const [
-              UserAccountsDrawerHeader(
-                currentAccountPicture: CircleAvatar(
-                  backgroundImage: AssetImage('assets/uncommon_sense.jpg'),
-                ),
-                accountName: Text('Jang'),
-                accountEmail: Text('wkdgkssk204@naver.com'),
-                decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 126, 94, 183),
-                    borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(40.0),
-                        bottomRight: Radius.circular(40.0))),
-              ),
-              ListTile(
-                leading: Icon(
-                  Icons.home,
-                  color: Colors.teal,
-                ),
-                title: Text('Home'),
-                trailing: Icon(Icons.add),
-              ),
-              ListTile(
-                leading: Icon(
-                  Icons.settings,
-                  color: Color.fromARGB(255, 88, 88, 88),
-                ),
-                title: Text('Setting'),
-                trailing: Icon(Icons.add),
-              ),
-              ListTile(
-                leading: Icon(
-                  Icons.question_answer,
-                  color: Colors.orange,
-                ),
-                title: Text('Q&A'),
-                trailing: Icon(Icons.add),
+          appBar: AppBar(
+            title: const Text('T A B  B A R'),
+            actions: [
+              IconButton(
+                icon: const Icon(Icons.search),
+                onPressed: () {
+                  print('search');
+                },
               ),
             ],
           ),
-        ),
-        body: Column(
-          children: [
-            TabBar(
+          drawer: Drawer(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: const [
+                UserAccountsDrawerHeader(
+                  currentAccountPicture: CircleAvatar(
+                    backgroundImage: AssetImage('assets/uncommon_sense.jpg'),
+                  ),
+                  accountName: Text('Jang'),
+                  accountEmail: Text('wkdgkssk204@naver.com'),
+                  decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 126, 94, 183),
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(40.0),
+                          bottomRight: Radius.circular(40.0))),
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.home,
+                    color: Colors.teal,
+                  ),
+                  title: Text('Home'),
+                  trailing: Icon(Icons.add),
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.settings,
+                    color: Color.fromARGB(255, 88, 88, 88),
+                  ),
+                  title: Text('Setting'),
+                  trailing: Icon(Icons.add),
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.question_answer,
+                    color: Colors.orange,
+                  ),
+                  title: Text('Q&A'),
+                  trailing: Icon(Icons.add),
+                ),
+              ],
+            ),
+          ),
+          body: Column(children: [
+            const TabBar(
               tabs: [
                 Tab(
                   icon: Icon(
@@ -91,18 +90,21 @@ class HomePage extends StatelessWidget {
                 ),
               ],
             ),
-            // TabBarView(children: [
-            //   Text('1st Tab'),
-            //   Center(
-            //     child: Text('2nd Tab'),
-            //   ),
-            //   Center(
-            //     child: Text('3rd Tab'),
-            //   ),
-            // ])
-          ],
-        ),
-      ),
+            TabBarView(children: [
+              Container(
+                  child: Center(
+                child: Text('1st Tab'),
+              )),
+              Container(
+                  child: Center(
+                child: Text('1st Tab'),
+              )),
+              Container(
+                  child: Center(
+                child: Text('1st Tab'),
+              )),
+            ]),
+          ])),
     );
   }
 }
