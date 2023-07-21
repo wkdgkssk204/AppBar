ead6eb5a692e7c9e3876ad3f94ab2568489d5c46
Jang Hanna <wkdgkssk204@naver.com>
2023-07-21 09:13:37 +0900

    first commit

---
+++ lib/homepage.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('T A B  B A R'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.search),
+              onPressed: () {
+                print('search');
+              },
+            ),
+          ],
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: const [
+              UserAccountsDrawerHeader(
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: AssetImage('assets/uncommon_sense.jpg'),
+                ),
+                accountName: Text('Jang'),
+                accountEmail: Text('wkdgkssk204@naver.com'),
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 126, 94, 183),
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(40.0),
+                        bottomRight: Radius.circular(40.0))),
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.home,
+                  color: Colors.teal,
+                ),
+                title: Text('Home'),
+                trailing: Icon(Icons.add),
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.settings,
+                  color: Color.fromARGB(255, 88, 88, 88),
+                ),
+                title: Text('Setting'),
+                trailing: Icon(Icons.add),
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.question_answer,
+                  color: Colors.orange,
+                ),
+                title: Text('Q&A'),
+                trailing: Icon(Icons.add),
+              ),
+            ],
+          ),
+        ),
+        body: Column(
+          children: [
+            TabBar(
+              tabs: [
+                Tab(
+                  icon: Icon(
+                    Icons.home,
+                    color: Colors.teal,
+                  ),
+                ),
+                Tab(
+                  icon: Icon(
+                    Icons.audiotrack,
+                    color: Colors.blue,
+                  ),
+                ),
+                Tab(
+                  icon: Icon(
+                    Icons.search,
+                    color: Color.fromARGB(255, 75, 75, 75),
+                  ),
+                ),
+                Tab(
+                  icon: Icon(
+                    Icons.favorite,
+                    color: Colors.pink,
+                  ),
+                ),
+              ],
+            ),
+            // TabBarView(children: [
+            //   Text('1st Tab'),
+            //   Center(
+            //     child: Text('2nd Tab'),
+            //   ),
+            //   Center(
+            //     child: Text('3rd Tab'),
+            //   ),
+            // ])
+          ],
+        ),
+      ),
+    );
+  }
+}
